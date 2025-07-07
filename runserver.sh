3fb5f98d58b031de374f09e0ab7b87876711f2f5
JOwOsh <j.j.lelipaly@gmail.com>
2025-07-07 16:09:56 +0200

    Added functionality to add options

--- runserver.sh
+++ runserver.sh
@@ -15,30 +15,40 @@ while IFS="=" read -r key value; do
     packs["$key"]="$value"
 done < <(jq -r 'to_entries | map("\(.key)=\(.value)") | .[]' "$database")
 
-# Check if the first argument is provided and is a valid pack
-if [[ ! -v "packs[$1]" ]]; then
-    echo "Usage: $0 <pack>"
-    exit 1
+# Runs the server for the specified pack
+run(){
+    # Check if the first argument is provided and is a valid pack
+    if [[ ! -v "packs[$1]" ]]; then
+        echo "Usage: $0 <pack>"
+        exit 1
+    fi
+
+    # check number of packs
+    cd "$workdir"
+    numpacks=$(ls -d */ | wc -l)
+    if [ "$numpacks" -ne "${#packs[@]}" ]; then
+        echo "Number of packs does not match the database."
+        echo "Perhaps you forgot to update the database?"
+        exit 1
+    fi
+
+    # change to the pack directory
+    packdir="${workdir}${packs[$1]}"
+    cd "$packdir"
+
+    # Check if run.sh exists in the pack directory
+    if [ ! "run.sh" ]; then
+        echo "run.sh not found in $packdir"
+        exit 1
+    fi
+
+    # Run the server
+    ./run.sh
+}
+
+# mode to run a pack
+if [[ "$1" == "run" ]]; then
+    shift
+    run "$@"
+    exit $? # Exit after running a pack
 fi
-
-# check number of packs
-cd "$workdir"
-numpacks=$(ls -d */ | wc -l)
-if [ "$numpacks" -ne "${#packs[@]}" ]; then
-    echo "Number of packs does not match the database."
-    echo "Perhaps you forgot to update the database?"
-    exit 1
-fi
-
-# change to the pack directory
-packdir="${workdir}${packs[$1]}"
-cd "$packdir"
-
-# Check if run.sh exists in the pack directory
-if [ ! "run.sh" ]; then
-    echo "run.sh not found in $packdir"
-    exit 1
-fi
-
-# Run the server
-./run.sh
